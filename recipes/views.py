fc904a8bc47fdd3a32649cd91766b580db0ab4c4
Saimor <saimorraduan@gmail.com>
2026-01-13 15:39:50 -0400

    Removido urls de contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
 
